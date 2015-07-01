be534e99aeca9744dd2cd6961361642e234dae2b
Pomin Wu <pomin5@gmail.com>
2015-07-02 03:40:52 +0800

    add python

--- macbook.sh
+++ macbook.sh
@@ -16,6 +16,16 @@ if [[ ! -x /usr/local/bin/brew ]]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+if [[ ! -d $HOME/.pyenv ]]; then
+    echo "Install pyenv"
+    git clone https://github.com/yyuu/pyenv.git $HOME/.pyenv
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+    pyenv install 2.7.10
+    pyenv global 2.7.10
+fi
+
 export PATH=/usr/local/bin:$PATH
 
 mkdir -p $SRC_DIRECTORY
@@ -23,6 +33,8 @@ mkdir -p $SRC_DIRECTORY
 if [[ ! -d $ANSIBLE_DIRECTORY ]]; then
     echo "Install Ansible"
     git clone git://github.com/ansible/ansible.git --recursive $ANSIBLE_DIRECTORY
+    cd $ANSIBLE_DIRECTORY
+    pip install six paramiko PyYAML Jinja2 httplib2
 fi
 
 source $ANSIBLE_DIRECTORY/hacking/env-setup > /dev/null
